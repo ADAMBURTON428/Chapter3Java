e6e7bb69148e4fb72085ed0bbacdbaea400ee1a6
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-13 09:52:44 -0500

    updated

--- Chapter3/src/youDoIt/ParadiseInfo2.java
+++ Chapter3/src/youDoIt/ParadiseInfo2.java
@@ -1,9 +1,22 @@
 package youDoIt;
 
+import java.util.Scanner;
+
 public class ParadiseInfo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		double price;
+		double discount;
+		double savings;
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Enter cutoff price for discout >> ");
+		price = keyboard.nextDouble();
+		
+		System.out.print("Enter discout rate as a whole a whole number >> ");
+		discount = keyboard.nextDouble();
+		
 
 	}
 
